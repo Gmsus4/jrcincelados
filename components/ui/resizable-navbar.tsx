6851f9ea12f934224160bca0ed8e4e4a11f56f65
Fernando <fernando452000@gmail.com>
2025-08-04 10:22:57 -0600

    Change favicon and metadata

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -251,7 +251,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="/jr.jpg"
+        src="/logo.webp"
         alt="logo"
         width={30}
         height={30}
